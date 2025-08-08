1a23f0423a4a341d534a93d66c9bdee2f656dc32
Mario Gemoll <mariogemoll@gmx.net>
2025-08-08 12:06:50 +0200

    Add a script for automatic code fixes

---
+++ sh/fix_code.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fail if a subcommand fails
+set -e
+
+# Print the commands
+set -x
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/../js
+
+npx eslint --fix
+
+cd $SCRIPT_DIR/../py
+
+isort .
+
+black --force-exclude 'seqs_pb2\.(py|pyi)$|\.ipynb$' .
+
+nbqa isort *.ipynb
+
+nbqa black *.ipynb
